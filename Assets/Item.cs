8533e022eb376166b64f7744567efc85b3c4bc39
I'm me <s179051@students.aie.edu.au>
2018-08-15 12:54:19 -0700

    added scrollrect

---
+++ Assets/Item.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item : MonoBehaviour {
+    public string itemName;
+    public float price;
+    public 
+    public Image icon;
+    public void Set(float newPrice, string newItemName = null, Image newIcon = null)
+    {
+        price = newPrice;
+        itemName = newItemName;
+        icon = newIcon;
+        icon.sprite
+    }
+}
