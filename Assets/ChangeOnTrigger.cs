c5866c47d34a3fa6513015b719394375d277117f
I'm me <s179051@students.aie.edu.au>
2018-08-21 18:00:28 -0700

    added cinemachine

---
+++ Assets/ChangeOnTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class ChangeOnTrigger : MonoBehaviour {
+    public CinemachineVirtualCamera defaultCamera;
+    public CinemachineVirtualCamera triggerCamera;
+    public int newPriority = 11;
+    public bool isPermanent = false;
+    private int oldPriority;
+    private void OnTriggerEnter(Collider other)
+    {
+        oldPriority = triggerCamera.Priority;
+        triggerCamera.Priority = newPriority;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (isPermanent == false)
+        {
+            triggerCamera.Priority = oldPriority;
+        }
+    }
+}
---
+++ Assets/Teleporter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+    public Teleporter Receiver;
+    public void TeleportHere(Collider traveler)
+    {
+        ignoreList.Add(traveler);
+        traveler.transform.position = transform.position;
+        traveler.transform.rotation = transform.rotation;
+    }
+    List<Collider> ignoreList = new List<Collider>();
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "UnityChan" && ignoreList.Contains(other) == false)
+        {
+            Receiver.TeleportHere(other);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        ignoreList.Remove(other);
+    }
+}
